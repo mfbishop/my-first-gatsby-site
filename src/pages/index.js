2a3b0aeaf57a0cf5d0d780935123e8075da98fa2
Jacek Biskup <jacekbiskup1@gmail.com>
2022-01-30 10:54:36 +0100

    Finished Gatsby tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Strona Lusi i Felci">
       <p>Witamy na stronie Lusi i Felci.</p>
+      <StaticImage
+        alt="zdjęcie Lusi i Felci"
+        src="../images/lif.jpg"
+      />
     </Layout>
   )
 }
